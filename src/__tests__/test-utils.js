3b54c3ca41678ecaa63288d6eefc3304d7971cfa
Björn Ekengren <bjorn@virtusize.com>
2018-10-07 16:23:55 +0200

    Fixed first test. Added some color to test output.

--- src/__tests__/test-utils.js
+++ src/__tests__/test-utils.js
@@ -1,6 +1,6 @@
 export function expect(label, condition) {
   if (!!condition) {
-    console.log(`Expect ${label}: PASS`);
+    console.log(`Expect ${label}: %cPASS`, 'color: #00a500;');
   } else {
     console.error(`Expect ${label}: FAIL`);
   }
--- src/js/robot.js
+++ src/js/robot.js
@@ -1,1 +1,7 @@
-export default {}
+function runCleaningRobot() {
+
+}
+
+export default {
+  runCleaningRobot
+}
