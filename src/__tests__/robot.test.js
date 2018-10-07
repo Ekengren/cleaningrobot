a1652c6c7db52f48e03e3019f671569a6b2b696d
Björn Ekengren <bjorn@virtusize.com>
2018-10-07 17:01:55 +0200

    Added more tests for relativeToAbsolute

--- src/__tests__/robot.test.js
+++ src/__tests__/robot.test.js
@@ -1,9 +1,21 @@
 import robot from '../js/robot'
-import { expect, relativeToAbsolute } from './test-utils'
+import { expect } from './test-utils'
 
 expect('robot to have function "runCleaningRobot"', typeof robot.runCleaningRobot === 'function');
 expect('robot to have function "relativeToAbsolute"', typeof robot.relativeToAbsolute === 'function');
+
 expect('function "relativeToAbsolute" to return an array', (function () {
   let result = robot.relativeToAbsolute({x:0, y:0}, ['N 2']);
   return Array.isArray(result);
+})());
+expect('function "relativeToAbsolute" to return correct number of vector objects', (function () {
+  let result = robot.relativeToAbsolute({x:0, y:0}, ['N 2', 'E 3', 'S 2']);
+  return result.length === 3;
+})());
+expect('function "relativeToAbsolute" to return correct vector objects', (function () {
+  let result = robot.relativeToAbsolute({x:0, y:0}, ['N 2', 'E 3', 'S 2']);
+  return result[1].x1 === 0 &&
+    result[1].y1 === 2 &&
+    result[1].x2 === 3 &&
+    result[1].y2 === 2;
 })());
