19148f080d69ef95172bd282c342e07d123a2245
Björn Ekengren <bjorn@virtusize.com>
2018-10-07 16:50:54 +0200

    Fixed test relativeToAbsolute

--- src/js/robot.js
+++ src/js/robot.js
@@ -1,7 +1,30 @@
-function runCleaningRobot() {
+/**
+ * Converts relative move commands to absolute vectors.
+ * @param startPosition [Object] format {x: integer, y: integer}
+ * @param relativeMoves [Array] Array of strings of format ['N 2', 'E 17']
+ * @returns [Object] Returns an array of vector objects [{x1: integer, y1: integer, x2: integer, y2: integer}]
+ */
+export function relativeToAbsolute(startPosition, relativeMoves) {
+  return [];
+}
+
+
+/**
+ *
+ * @param params [Object] Instruction for cleaning robot. Example:
+ * {
+ *  numberOfCommands: 2,
+ *  startPosition: {10, 22},
+ *  moves: [
+ *    'E 2', 'N 1'
+ *  ],
+ * }
+ */
+function runCleaningRobot(params) {
 
 }
 
 export default {
-  runCleaningRobot
+  runCleaningRobot,
+  relativeToAbsolute
 }
