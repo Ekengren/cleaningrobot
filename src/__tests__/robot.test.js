eb387111135d003364162791f5b203745c1433fd
Björn Ekengren <bjorn@virtusize.com>
2018-10-07 16:50:20 +0200

    Added test for relativeToAbsolute vector conversion function

--- src/__tests__/robot.test.js
+++ src/__tests__/robot.test.js
@@ -1,5 +1,9 @@
 import robot from '../js/robot'
-import { expect } from './test-utils'
+import { expect, relativeToAbsolute } from './test-utils'
 
 expect('robot to have function "runCleaningRobot"', typeof robot.runCleaningRobot === 'function');
 expect('robot to have function "relativeToAbsolute"', typeof robot.relativeToAbsolute === 'function');
+expect('function "relativeToAbsolute" to return an array', (function () {
+  let result = robot.relativeToAbsolute({x:0, y:0}, ['N 2']);
+  return Array.isArray(result);
+})());
--- src/__tests__/test-utils.js
+++ src/__tests__/test-utils.js
@@ -1,7 +1,11 @@
 export function expect(label, condition) {
-  if (!!condition) {
-    console.log(`Expect ${label}: %cPASS`, 'color: #00a500;');
-  } else {
+  try {
+    if (!!condition) {
+      console.log(`Expect ${label}: %cPASS`, 'color: #00a500;');
+    } else {
+      console.log(`Expect ${label}: %cFAIL`, 'color: #f00000;');
+    }
+  } catch (e) {
     console.log(`Expect ${label}: %cFAIL`, 'color: #f00000;');
   }
 }
